6a62fc22164a4a263f07f8e123cb5aae829763c0
Stefany Alabbadi <sfalabba@iu.edu>
2024-01-25 16:42:34 +0000

    first commit

--- Distance.js
+++ Distance.js
@@ -1,23 +1,15 @@
-// function 1
-
 function feetToMeter (feet) {
     return feet * 0.3048;
 }
-
-//function 2
 function metersToFeet (meters){
     return meters * 3.28084;
 }
-
-// function 3
 function milesToKm (miles){
     return miles * 1.60934;
 }
-//function 4
 function kmToMiles (km) {
     return km * 0.621371;
  }
-//function 5 calculate
 function calculate(unit, value) {
     switch (unit) {
         case 'feet':
@@ -32,6 +24,4 @@ function calculate(unit, value) {
             return 'Invalid unit';
     }
 }
-
-// Export the calculate function
 module.exports = calculate;
